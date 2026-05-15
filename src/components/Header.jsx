7484af090194f969908993750e889f0e6542c5dd
Crystal Mucheru <crystalmucheru@gmail.com>
2026-05-15 10:03:28 +0300

    Add Header component with logo image

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div id="toy-header">
+      <img
+        src="https://fontmeme.com/permalink/180719/674291b52f115e516331273d31704e63.png"
+        alt="toy header"
+      />
+    </div>
+  );
+}
+
+export default Header;
