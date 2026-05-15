7ffbb2cb16d210439188fae03305f1787918c659
Crystal Mucheru <crystalmucheru@gmail.com>
2026-05-15 10:05:52 +0300

    Add ToyForm component for creating new toys

---
+++ src/components/ToyForm.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+function ToyForm({ onAddToy }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newToy = {
+      name,
+      image,
+      likes: 0,
+    };
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newToy),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        onAddToy(data);
+        setName("");
+        setImage("");
+      });
+  }
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="add-toy-form">
+        <h3>Create a toy!</h3>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter a toy's name..."
+          className="input-text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          name="image"
+          placeholder="Enter a toy's image URL..."
+          className="input-text"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <br />
+        <input
+          type="submit"
+          name="submit"
+          value="Create New Toy"
+          className="submit"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default ToyForm;
