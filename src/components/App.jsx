90b9348771e31ebec899cca03447a463453a9cf9
Crystal Mucheru <crystalmucheru@gmail.com>
2026-05-15 10:02:37 +0300

    Implement toy management features in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import ToyForm from "./ToyForm";
+import ToyContainer from "./ToyContainer";
 
+function App() {
+  const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((r) => r.json())
+      .then((data) => setToys(data));
+  }, []);
+
+  function handleToggleForm() {
+    setShowForm((showForm) => !showForm);
+  }
+
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
+  function handleDeleteToy(id) {
+    const updatedToys = toys.filter((toy) => toy.id !== id);
+    setToys(updatedToys);
+  }
+
+  function handleUpdateToy(updatedToy) {
+    const updatedToys = toys.map((toy) =>
+      toy.id === updatedToy.id ? updatedToy : toy
+    );
+    setToys(updatedToys);
+  }
+
+  return (
+    <>
+      <Header />
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
+      <div className="buttonContainer">
+        <button onClick={handleToggleForm}>Add a Toy</button>
+      </div>
+      <ToyContainer 
+        toys={toys} 
+        onDeleteToy={handleDeleteToy} 
+        onUpdateToy={handleUpdateToy} 
+      />
+    </>
+  );
+}
+
+export default App;
