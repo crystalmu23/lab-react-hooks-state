3b5cd844a2244982959aed42d079bec81255332f
Crystal Mucheru <crystalmucheru@gmail.com>
2026-05-15 10:05:03 +0300

    Add ToyContainer component to display toy collection

---
+++ src/components/ToyContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ToyCard from "./ToyCard";
+
+function ToyContainer({ toys, onDeleteToy, onUpdateToy }) {
+  return (
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard 
+          key={toy.id} 
+          toy={toy} 
+          onDeleteToy={onDeleteToy} 
+          onUpdateToy={onUpdateToy} 
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ToyContainer;
